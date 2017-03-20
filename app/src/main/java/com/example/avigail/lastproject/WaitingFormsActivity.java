9230b0a514b644f3ee58f628b083415a7062f233
avigailab <avigailavraham7@gmail.com>
2017-03-20 08:20:59 +0200

    display all waitings form in waiting form activity

--- app/src/main/java/com/example/avigail/lastproject/WaitingFormsActivity.java
+++ app/src/main/java/com/example/avigail/lastproject/WaitingFormsActivity.java
@@ -21,6 +21,7 @@ public class WaitingFormsActivity extends ListActivity {
     TextView content;
     ListView listView;
     public static final String MY_PREFS_NAME = "MyPrefs";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -29,26 +30,15 @@ public class WaitingFormsActivity extends ListActivity {
 
         SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
         Map<String,?> keys = prefs.getAll();
+        String[] values=new String[keys.size()];
+        int i=0;
         for(Map.Entry<String,?> entry : keys.entrySet()){
             Log.d("map values",entry.getKey() + ": " + entry.getValue().toString());
+            values[i]= entry.getKey();
+            i++;
         }
-       /* Gson gson = new Gson();
-        String json = prefs.getString("MyObject", "");
-        Layout obj = gson.fromJson(json, Layout.class);*/
-
-       /* SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
-        String restoredText = prefs.getString("text", null);
-        if (restoredText != null) {
-            String name = prefs.getString("name", "No name defined");//"No name defined" is the default value.
-            int idName = prefs.getInt("idName", 0); //0 is the default value.
-        }*/
-
-        /*listView = (ListView) findViewById(android.R.id.list);
+        listView = (ListView) findViewById(android.R.id.list);
         content = (TextView)findViewById(R.id.output);
-        WaitingForms waitingForms=new WaitingForms();
-        final ArrayList<Layout> arrayOfLayouts=waitingForms.getWaitingForms();
-        Log.i("waaaaa", String.valueOf(waitingForms.getWaitingForms().get(0).layoutName));
-        String[] values = new String[] {""};
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
@@ -57,7 +47,6 @@ public class WaitingFormsActivity extends ListActivity {
         //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         listView.setAdapter(adapter);
-
         //********************************
         // Inflate the layout for this fragment
         //add click
@@ -65,13 +54,8 @@ public class WaitingFormsActivity extends ListActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Log.e("---", "click " + i);
-                Intent layoutIntent = new Intent(getApplication(), FormActivity.class);
-                //pass current layout
-                layoutIntent.putExtra("LAYOUT",arrayOfLayouts.get(i));
-                startActivity(layoutIntent);
-
             }
-        });*/
+        });
     }
 
 
