c8091282eaeb37e9e5f263b1d1fec134f3e8b47a
avigailab <avigailavraham7@gmail.com>
2017-03-21 16:40:11 +0200

    remove settings tab

--- app/src/main/java/com/example/avigail/lastproject/MainActivity.java
+++ app/src/main/java/com/example/avigail/lastproject/MainActivity.java
@@ -112,7 +112,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
@@ -123,10 +123,7 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
